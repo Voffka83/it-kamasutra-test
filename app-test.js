5094121e55d829df2bd6641089ae600d86fcb85e
Voffka Dubinkin <vl.dubinkin@gmail.com>
2022-02-07 19:18:27 +0100

    First commit

---
+++ app-test.js
@@ -0,0 +1,27 @@
+
+
+let scores = [60, 62, 45, 32, 34, 56, 76, 32, 54, 78, 24, 23, 37, 47, 49, 94, 93, 84, 75]
+let output;
+for (let i = 0; i < scores.length; i++) {
+	output = "Bubble solution #" + i + " score: " + scores[i];
+	console.log(output);
+}
+
+
+
+
+
+/*setTimeout(wakeUpUser, 3000);
+function wakeUpUser() {
+	alert('яндекс круче всех, но это однозначно не точно')
+}
+//это просто alert без задания условия поиска, обычный вывод на страницу предупреждения с задержкой в 3 секунды
+*/
+
+
+/*
+// 6. функция superSum двух произвольных чисел
+let a = +prompt("Введите первое число", "");
+let b = +prompt("Введите второе число", "");
+
+alert(a + b);*/
